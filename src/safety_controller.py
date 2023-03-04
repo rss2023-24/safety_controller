845c1d32bc804c748071a4ab76e7a5aaceb381d2
hgheiberger <harryh@mit.edu>
2023-03-04 16:24:26 +0000

    Initialize safety_controller node

---
+++ src/safety_controller.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2
+
+import numpy as np
+
+import rospy
+import math
+import sensor_msgs.point_cloud2 as pc2
+from rospy.numpy_msg import numpy_msg
+from sensor_msgs.msg import LaserScan, PointCloud2
+import laser_geometry.laser_geometry as lg
+from std_msgs.msg import Header
+from ackermann_msgs.msg import AckermannDrive, AckermannDriveStamped
+from visualization_tools import *
+
+class SafetyController:
+    LASER_SCAN_TOPIC = "/scan"
+    NAV_OUTPUT_TOPIC = "/vesc/high_level/ackermann_cmd_mux/output"
+    SAFETY_DRIVE_TOPIC = "/vesc/low_level/ackermann_cmd_mux/input/safety"
+
+
+    def __init__(self):
+        # Initializes subscribers
+        rospy.Subscriber(self.LASER_SCAN_TOPIC, LaserScan, self.handleSafety)
+        rospy.Subscriber(self.NAV_OUTPUT_TOPIC, AckermannDriveStamped, self.handleSafety)
+
+        # Initializes publishers
+        self.pub = rospy.Publisher(self.SAFETY_DRIVE_TOPIC, AckermannDriveStamped) 
+
+        # Configurable Parameters
+        # TODO
+
+    # Handles safety controller logic
+    def handleSafety(self, laser_scan):
+        pass
+
+    # Closest distance from origin to a line with equation y = mx + b
+    def distanceToLine(self, m, b):
+        return abs(b)/(math.sqrt(m*m + 1))
+
+    # Sends control commands to the robot
+    def controlRobot(self, speed, steering_angle, steering_angle_velocity=0, acceleration=0, jerk=0 ):
+        drive_command_stamped = AckermannDriveStamped()
+        
+        # Builds command header
+        drive_header = Header()
+        drive_header.stamp = rospy.Time.now() 
+        drive_header.frame_id = "base_link"
+        drive_command_stamped.header = drive_header
+
+        # Builds drive command
+        drive_command = AckermannDrive()
+        drive_command.speed = speed
+        drive_command.steering_angle = steering_angle
+        drive_command.steering_angle_velocity = steering_angle_velocity
+        drive_command.acceleration = acceleration
+        drive_command.jerk = jerk
+        drive_command_stamped.drive = drive_command
+
+        # Publishes message
+        self.pub.publish(drive_command_stamped)
+
+
+
+if __name__ == "__main__":
+    rospy.init_node('safety_controller')
+    safety_controller = SafetyController()
+    rospy.spin()
