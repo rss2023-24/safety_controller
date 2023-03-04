8b943f201bf7b4f2a2c56e5d164609274d64be52
hgheiberger <harryh@mit.edu>
2023-03-04 16:31:25 +0000

    Set up subscribers and publishers

--- src/safety_controller.py
+++ src/safety_controller.py
@@ -20,8 +20,10 @@ class SafetyController:
 
     def __init__(self):
         # Initializes subscribers
-        rospy.Subscriber(self.LASER_SCAN_TOPIC, LaserScan, self.handleSafety)
-        rospy.Subscriber(self.NAV_OUTPUT_TOPIC, AckermannDriveStamped, self.handleSafety)
+        self.laser_data = None
+        self.last_drive_command = None
+        rospy.Subscriber(self.LASER_SCAN_TOPIC, LaserScan, self.gatherLaserData)
+        rospy.Subscriber(self.NAV_OUTPUT_TOPIC, AckermannDriveStamped, self.gatherDriveCommand)
 
         # Initializes publishers
         self.pub = rospy.Publisher(self.SAFETY_DRIVE_TOPIC, AckermannDriveStamped) 
@@ -29,35 +31,20 @@ class SafetyController:
         # Configurable Parameters
         # TODO
 
+    # Gather laser scan data
+    def gatherLaserData(self, laser_scan):
+        self.laser_data = laser_scan
+        self.handleSafety()
+
+    # Gather last drive command
+    def gatherDriveCommand(self, last_drive_command):
+        self.last_drive_command = last_drive_command
+        self.handleSafety()
+
     # Handles safety controller logic
-    def handleSafety(self, laser_scan):
+    def handleSafety(self):
         pass
 
-    # Closest distance from origin to a line with equation y = mx + b
-    def distanceToLine(self, m, b):
-        return abs(b)/(math.sqrt(m*m + 1))
-
-    # Sends control commands to the robot
-    def controlRobot(self, speed, steering_angle, steering_angle_velocity=0, acceleration=0, jerk=0 ):
-        drive_command_stamped = AckermannDriveStamped()
-        
-        # Builds command header
-        drive_header = Header()
-        drive_header.stamp = rospy.Time.now() 
-        drive_header.frame_id = "base_link"
-        drive_command_stamped.header = drive_header
-
-        # Builds drive command
-        drive_command = AckermannDrive()
-        drive_command.speed = speed
-        drive_command.steering_angle = steering_angle
-        drive_command.steering_angle_velocity = steering_angle_velocity
-        drive_command.acceleration = acceleration
-        drive_command.jerk = jerk
-        drive_command_stamped.drive = drive_command
-
-        # Publishes message
-        self.pub.publish(drive_command_stamped)
 
 
 
